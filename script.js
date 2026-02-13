6c49d110605765c093c7e0bccea1203ecb0d05f0
Medha Jain <medhajain790@gmail.com>
2026-02-13 14:53:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 const addEventBtn = document.getElementById("addEvent");
 const eventList = document.getElementById("eventList");
 
-// Add Event on Button Click
 addEventBtn.addEventListener("click", function () {
 
-    // Get input values
     const title = document.getElementById("title").value.trim();
     const date = document.getElementById("date").value;
     const category = document.getElementById("category").value;
     const description = document.getElementById("description").value.trim();
-
-    // Validation
+    
     if (title === "" || date === "") {
         alert("Please fill Event Title and Date");
         return;
     }
 
-    // Create Event Card
     const card = document.createElement("div");
     card.className = "event-card";
 
-    // Create elements separately (better DOM practice)
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "X";
     deleteBtn.className = "delete-btn";
@@ -37,24 +32,19 @@ addEventBtn.addEventListener("click", function () {
     const eventDesc = document.createElement("p");
     eventDesc.innerText = description;
 
-    // Append elements to card
     card.appendChild(deleteBtn);
     card.appendChild(eventTitle);
     card.appendChild(eventDate);
     card.appendChild(eventCategory);
     card.appendChild(eventDesc);
 
-    // Append card to right section
     eventList.appendChild(card);
 
-    // Clear form after submission
     document.getElementById("title").value = "";
     document.getElementById("date").value = "";
     document.getElementById("description").value = "";
 });
 
-
-// Event Delegation for Delete
 eventList.addEventListener("click", function (e) {
     if (e.target.classList.contains("delete-btn")) {
         const card = e.target.parentElement;
@@ -63,7 +53,6 @@ eventList.addEventListener("click", function (e) {
 });
 const clearAllBtn = document.getElementById("clearAll");
 
-// Clear All Events
 clearAllBtn.addEventListener("click", function () {
 
     if (eventList.children.length === 0) {
